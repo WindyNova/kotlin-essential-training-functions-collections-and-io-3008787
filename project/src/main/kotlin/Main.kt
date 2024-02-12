0d919763a5a1e4188dde54a56aa3a6f6be3872e3
pyth0nio <119175385+pyth0nio@users.noreply.github.com>
2024-02-12 23:00:19 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,25 @@
 
 fun main() {
-    
+    val aInt:Int =0
+    val anotherInt = 0
+    val aByte:Byte = 0
+    val aShort:Short=0
+    val aLong:Long = 0
+    val inferdInt = 10
+    val interedLong = 10000000000
+    val anotherLong = 10L
+    val unsignedInt :UInt =0U
+    val unsignedong = 0UL
+
+    val aDouble:Double =5.5
+    val aFloat:Float = 5.5f
+    val inferredDou= 5.5
+
+val inferedFLoat = 5.5f
+    println (10f>10)
+    println(10.5<=5.2)
+    println(10.5.toInt())
+    5.toULong()
+
+    println(100_000_000.toByte())
 }
