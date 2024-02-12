f2ad01b3fb5d9e7258767694d87d8b688fabdadb
pyth0nio <119175385+pyth0nio@users.noreply.github.com>
2024-02-12 23:12:37 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
 
 fun main() {
-    
+    val aChar :Char = 'a'
+    val aNum ='1'
+    val nb = '\n'
+
+    val concatt = "ab"
+    val aString : String = "Hello kot\n"
+   val escapae = "Hello \n World"
+    val raww= """
+        SOme multi line over here hehe
+    """.trimIndent()
+    val cat =" Hello" + "Sttrnng"
+    val concag =" The nuber" + 10
+     val aNumber =10
+  val ok  = " The number 1${aNumber}"
+ok.contains("Kotlin")
 }
