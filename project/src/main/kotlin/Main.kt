08eb7b78fde478fde3de396f21aab3f84171397e
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-13 22:42:46 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -5,7 +5,12 @@
 // function takes 2 strings for first and last name
 // function takes a formatting function parameter
 // function should print the result of the passed format function
+fun thisisfun(valu1:String,valu2:String,formatter :(String,String) ->String)
+{
+    println(formatter(valu1,valu2))
+}
 
 fun main() {
-
+thisisfun("John","Doe") { ok, ok1 -> "$ok,$ok1" }
 }
+
