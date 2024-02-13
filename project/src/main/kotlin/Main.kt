35385aea6235672a49fec0b1aa5a60d3e5a2d834
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-13 22:35:19 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
-
+fun printCalc(value1:Int,value2:Int,calculator:(Int,Int)->Int){
+   println("The value is : ${calculator(value1,value2)}")
+}
 
 fun main() {
-    
+    printCalc(2,2) { value1, valu2 -> value1 + valu2 }
+    printCalc(2,2){
+        value1,valu2-> value1-valu2
+    }
 }
