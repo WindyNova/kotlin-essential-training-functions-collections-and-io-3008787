31fed5989c8ee869bb3e70b0d9eea8b86da4b94c
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-14 23:01:54 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
 
 fun main() {
+    var counter = 0
+    while (counter < 5) {
+        print(counter)
+        counter++
+        if (counter == 3) break
+    }
+    do {
+        println(counter)
+        counter+=1
+
+    } while (counter<5)
+
 
 }
