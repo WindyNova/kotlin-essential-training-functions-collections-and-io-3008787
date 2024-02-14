b88fc7d92cd2f678d2ec0d8df6f4b388c5e4e434
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-14 22:44:24 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,28 @@
 
-
+fun getJess(input:Int):String{
+    return if (input>3){
+        "nonono"
+    }
+    else{
+        "okokokok"
+    }
+}
 fun main() {
-    
+    var somevarisb=0
+    val message = if (somevarisb>3)
+    {
+        "vallll"
+    }
+    else
+
+    {
+        "nononon"
+    }
+    println(message)
+
+
+    when(somevarisb)
+    {
+        3-> "okokokokokoko"
+    }
 }
