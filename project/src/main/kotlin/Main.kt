fdae4822d423f981b253d0193d337d34a6c6f606
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-16 16:13:15 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 
-fun main() {
-    
-}
+fun main(vararg args:String) {
+    if (args.isEmpty())
+    {
+        print("lolz")
+    }
+    println("First:  ${args[0]}")
+    for (arg in args) println(arg)
+    }
+
