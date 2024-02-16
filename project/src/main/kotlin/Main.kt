ebb0b749d5c101b9e0117b67dee6120471cdd2d9
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-16 16:08:40 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@
 fun main() {
     // Using the provided test scores, identify the 3 students
     // with the lowest test scores
-
+println(testScores.toList().sortedBy{it.second
+}.take(3))
     
 }
