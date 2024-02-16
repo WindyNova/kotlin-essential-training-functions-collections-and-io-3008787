807ca4896a2b365d6322f1b94507bf87927f9daf
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-16 16:14:29 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
+import java.io.File
 
 fun main() {
-    
+    print("Enter ")
+val filename = readLine()?:""
+    println("filename is $filename")
+    if(File(filename).isFile)
+    {
+        println("okok")
+    }
 }
