bd9b3488cabf1b2bed93cdc939f598292e4b62a8
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-16 15:40:24 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 
 fun main() {
-    
+    val readOnlyList = listOf(1,2,3)
+    readOnlyList.first()
+    readOnlyList.take(3)
+    readOnlyList.take(2)
+    val mapp= mapOf(1 to "ok",2 to "ban")
+   print(mapp.filter { it.key<2 })
+    readOnlyList.filter { it>1 }
+
 }
