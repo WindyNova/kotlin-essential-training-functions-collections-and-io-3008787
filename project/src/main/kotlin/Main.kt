4e7402316252d9ea1ad7d3ab62b141bb4694f865
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-16 16:02:32 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 
 fun main() {
-    
+    val language = listOf("kotlin").filter { it.length>4 }.map { it.length }
+
+
+    // Sequence
+    val  sequance = sequenceOf("kotlin","javava")
+    sequance.iterator().next().length.toByte()
+    val aSSSequance = listOf(1,2,3,4).asSequence()
+
 }
