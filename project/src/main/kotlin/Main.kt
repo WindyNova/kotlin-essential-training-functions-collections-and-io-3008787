e05527a6b65782643ddd61f729e85811b8becf61
WindyNova <42537968+WindyNova@users.noreply.github.com>
2024-02-16 15:34:53 +0000

    ok

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 
 fun main() {
-
+val language = mutableSetOf("kotlon")
+    println(language)
+    language.add("javascrpt")
+    language.remove("kotlin")
+    val map1 = mapOf(Pair(123,"ok"),789 to "123")
+    println(map1.entries.first())
+    println(map1[123])
 }
